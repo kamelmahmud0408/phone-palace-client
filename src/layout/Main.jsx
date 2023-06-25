e8c8b50f6f740c44df0fec23ffb1d362568011a3
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-25 18:33:03 +0600

    added navbar

---
+++ src/layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/Sheared/Navbar/Navbar';
+import Footer from '../pages/Sheared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
