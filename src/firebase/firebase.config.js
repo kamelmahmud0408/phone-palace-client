b2bd65233f7f8414837c2e35b8254bf384ba1a29
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-25 18:49:06 +0600

    added footer and install firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC57yX5HoGW4U-euIXSk4u3cclUSEclReQ",
+  authDomain: "phone-palace.firebaseapp.com",
+  projectId: "phone-palace",
+  storageBucket: "phone-palace.appspot.com",
+  messagingSenderId: "724907456955",
+  appId: "1:724907456955:web:0ed4df0734f8582b3dac9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
